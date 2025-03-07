baf626a12f906c8e19794af0cc70784d670230ac
Snigdha Singh <singhsnigdha2002@gmail.com>
2025-03-07 15:38:00 +0530

    test-commit

--- src/agent.mjs
+++ src/agent.mjs
@@ -2,8 +2,6 @@ import { Octokit } from "@octokit/rest";
 import {GoogleGenerativeAI} from "@google/generative-ai";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
-// import os from "os";
-// import dotenv from "dotenv";
 
 //const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 //const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
